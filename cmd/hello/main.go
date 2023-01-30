969ae999d3a55cf1739b9d134566a49554cc92ad
Jessica Lin <66900456+jessicalin94@users.noreply.github.com>
2023-01-30 16:07:56 +1100

    Variables, constants and types

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -2,10 +2,62 @@ package main
 
 import (
 	"fmt"
-
 	"golearn/internal/hello"
+	"math/cmplx"
 )
+
+// variables defined at package level (type at end)
+var c, python, java bool
+
+// variables defined with initialisers
+var a, b int = 1, 2
+
+// basic types (+ a block of variable declarations)
+var (
+	ToBe bool = false
+	MaxInt uint64 = 1<<64 - 1
+	z complex128 = cmplx.Sqrt(-5 + 21i)
+)
+
+// numeric constants
+const (
+	Big = 1 << 100
+	Small = Big >> 99
+)
+func needInt(x int) int { return x*10 + 1 }
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
 
 func main() {
 	fmt.Println(hello.SayHello("world"))
+
+	fmt.Println("\nVariables:")
+	// variables defined at function level
+	var i int
+
+	// variables defined without type, but inferred from init
+	var javascript, golang = true, "no!"
+
+	// short variable declations (can only be used inside functions)
+	c := 3		// same as var c = 3
+	fmt.Println(i, c, python, java, a, b, javascript, golang, c)
+
+	fmt.Println("\nSome basic types:")
+	fmt.Printf("ToBe Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("MaxInt Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("z Type: %T Value: %v\n", z, z)
+
+	fmt.Println("Zero values:", hello.ZeroValues())
+	fmt.Println("Type conversion:", hello.TypeConversion())
+	fmt.Println("Type inference:", hello.TypeInference())
+
+	fmt.Println("Constants:", hello.Constants())
+
+	fmt.Println("\nNumeric constants:")
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+	// fmt.Println(needInt(Big))  // throws error as the value of 'Big' is too large
+
 }
--- internal/hello/hello.go
+++ internal/hello/hello.go
@@ -2,8 +2,44 @@ package hello
 
 import (
 	"fmt"
+	"math"
 )
+
+// constants
+const Pi = 3.14
 
 func SayHello(name string) string {
 	return fmt.Sprintf("Hello %s!", name)
+}
+func ZeroValues() string {
+	// Zero values when no initial value given on declaration
+	var i int		// numeric = 0
+	var f float64	// 0
+	var b bool		// false
+	var s string	// ""
+	return fmt.Sprintf("%v %v %v %q\n", i, f, b, s)
+}
+
+func TypeConversion() string {
+	// Type conversion
+	// T(v) converts the value to the type T 
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	return fmt.Sprintln(x, y, z)
+}
+
+func TypeInference() string {
+	// When declaring an untyped numeric constant, it'll be inferred by the precision of the value declared
+	v := 42.1i
+	return fmt.Sprintf("v is of the type %T\n", v)
+}
+
+func Constants() string {
+	// declared using the 'const' keyword
+	// can be character, string, boolean, numeric
+	// cannot use shorthand to declare constants ( := )
+	const World = "世界"
+	return fmt.Sprintln("Hello", World, "Happy", Pi, "Day")
+	return fmt.Sprintln("Happy", Pi, "Day")
 }
